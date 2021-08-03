667e0beef12a6eaeb84c7c490a122e2da26ab1f1
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2021-08-03 16:53:47 -0500

    responsive design

--- cart.js
+++ cart.js
@@ -54,27 +54,55 @@ function generateCartItems(cartItems){
 	let itemsHTML = "";
 	cartItems.forEach((item)=> {
 		itemsHTML += `
-		<div class="cart-item flex items-center pb-4 border-b border-gray-100">
-			<div class="cart-item-image w-40 h-24 bg-white p-4 rounded-lg"><img class="w-full h-full object-contain"  src="${item.image}" alt="laptop"/>
-			</div>
-			<div class="cart-item-details flex-grow">
-			<div   class="    cart-item-details-title    font-bold    text-sm text-gray-600  ">  ${item.name}
-			</div>
-                	<div class="cart-item-brand text-sm text-gray-400">${item.make}</div></div>
-			<div class="cart-item-counter w-48 flex items-center text-gray-400">
-                		<div  data-id="${item.id}" class="    cart-item-decrease     cursor-pointer    bg-gray-100    rounded    h-6    w-6    flex    justify-center    items-center    hover:bg-gray-200    mr-2  ">
-				<i class="fas fa-chevron-left fa-xs"></i>
+		<div class="cart-item flex flex-wrap items-center pb-4 border-b border-gray-300 mb-4">
+			<div class="cart-item-details flex flex-grow flex-col">
+				<h2 class="font-bold table-titles text-gray-400 items-start">
+						Product
+				</h2>
+				<div class="cart-item-details flex flex-grow flex-wrap">
+
+					<div class="cart-item-image w-38 h-24 bg-white p-4 rounded-lg">
+						<img class="w-full h-full object-contain"  src="${item.image}" alt="laptop"/>
+					</div>
+					<div class="cart-item-data flex flex-col justify-center">
+					<div   class="    cart-item-details-title    font-bold    text-sm text-gray-600  ">  
+						${item.name}
+					</div>
+                			<div class="cart-item-brand text-sm text-gray-400">
+						${item.make}
+						</div>
+					</div>
 				</div>
-				<h4 class="text-gray-400">${item.quantity}</h4>
-                		<div data-id="${item.id}" class="    cart-item-increase    cursor-pointer    bg-gray-100    rounded    h-6    w-6    flex    justify-center    items-center    hover:bg-gray-200    ml-2  "> 
-				<i class="fas fa-chevron-right fa-xs"></i>
+			</div>
+			<div class="cart-item-counter-box w-48 flex flex-wrap items-center text-gray-400">
+				<h2 class="font-bold table-titles text-gray-400 flex items-start">
+					Count
+				</h2>
+				<div class="cart-item-counter w-40 h-20 flex items-center text-gray-400">
+                			<div  data-id="${item.id}" class="    cart-item-decrease     cursor-pointer    bg-gray-100    rounded    h-6    w-6    flex    justify-center    items-center    hover:bg-gray-200    mr-2  ">
+						<i class="fas fa-chevron-left fa-xs"></i>
+					</div>
+					<h4 class="text-gray-400">
+					${item.quantity}
+					</h4>
+                			<div data-id="${item.id}" class="    cart-item-increase    cursor-pointer    bg-gray-100    rounded    h-6    w-6    flex    justify-center    items-center    hover:bg-gray-200    ml-2  "> 
+						<i class="fas fa-chevron-right fa-xs"></i>
+					</div>
 				</div>
 			</div>
-			<div data-id="${item.id}" class="cart-item-total-cost w-48 font-bold text-gray-400">
-			${numeral(item.price * item.quantity).format("$0,0.00")}
+			<div class="cart-item-total-cost-box w-48 flex flex-wrap items-center text-gray-400">
+				<h2 class="font-bold table-titles text-gray-400 flex items-start">
+					Total Cost
+				</h2>
+				<div data-id="${item.id}" class="cart-item-total-cost w-48 h-20 flex items-center font-bold text-gray-400">
+					${numeral(item.price * item.quantity).format("$0,0.00")}
+				</div>
 			</div>
-			<div data-id="${item.id}" class="   cart-item-delete   w-10  font-bold  text-gray-300  cursor-pointer  hover:text-gray-400">
-			<i class="fas fa-times"></i>
+			<div class="cart-item-delete-box w-10 flex flex-wrap items-center text-gray-400">
+				<h2 class="table-titles text-transparent flex items-start">Delete</h2>
+				<div data-id="${item.id}" class="   cart-item-delete   w-10  h-20 flex items-center font-bold  text-gray-300  cursor-pointer  hover:text-gray-400">
+					<i class="fas fa-times"></i>
+				</div>
 			</div>
 		</div>
 		`
